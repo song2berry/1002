6929155f750c041ecbfb96e2ecbb241d523743bf
sipossa <87564335+sipossa@users.noreply.github.com>
2021-10-20 15:27:16 +0900

    Add files via upload

--- 20211019/js/training008.js
+++ 20211019/js/training008.js
@@ -7,7 +7,7 @@ $('#top_pop i').on('click', function(){
 
 $('.main_slider').slick({
     arrows:false,
-    dots:true,
+    //dots:true,
     autoplay:true,
     pauseOnHover:false,
 });
@@ -17,5 +17,13 @@ $('.main_slider figure').eq(1).addClass('on');
 $('.main_slider').on('afterChange', function(e,s,c){
     $('.main_slider figure').eq(c+1).addClass('on').siblings().removeClass('on');
 });
+
+$('#con04 i.xi-pause').on('click', function(){
+    $('#mv01').trigger('pause');
+});
+$('#con04 i.xi-play').on('click', function(){
+    $('#mv01').trigger('play');
+});
+
 //----------------------------------------------
 });
